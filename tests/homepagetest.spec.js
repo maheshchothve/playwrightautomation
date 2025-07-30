0c89135f6b66fa4d343ef9e5fef2f46dc11c11f8
mahesh chothve <maheshchothve1997@gmail.com>
2025-07-30 16:25:24 +0530

    commiting new changes in the homepagetest case

--- tests/homepagetest.spec.js
+++ tests/homepagetest.spec.js
@@ -1,13 +1,19 @@
  const {test,expect} =require('@playwright/test')
 
 test('Home Page',async ({page})=>{
-    await page.goto("https://www.demoblaze.com/");
+    await page.goto("https://www.automationexercise.com/");
 
     const pagetitle= await page.title();
     console.log("title of the page is"+pagetitle);
 
-    await expect(page).toHaveTitle('STORE');
+    await expect(page).toHaveTitle('Automation Exercise');
 
-    await page.close();
+    await page.locator('//div[@class="shop-menu pull-right"]/ul/li[4]').click();
+
+    console.log("clicked on the singup/login link");
+    const logintitle=await page.title();
+    console.log("title after clicking "+logintitle);
+
+    await expect(page).toHaveTitle('Automation Exercise - Signup / Login');
 
 } )
