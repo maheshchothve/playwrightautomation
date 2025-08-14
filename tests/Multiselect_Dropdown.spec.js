d07cbf03c604da9e4b4ccf948ec90f763d4aaf8e
mahesh chothve <maheshchothve1997@gmail.com>
2025-08-14 18:48:11 +0530

    added hidden dropdown

--- tests/Multiselect_Dropdown.spec.js
+++ tests/Multiselect_Dropdown.spec.js
@@ -4,5 +4,25 @@ test('handle check box',async({page})=>{
    await  page.goto("https://testautomationpractice.blogspot.com/")
     
    await page.selectOption('#colors',['red','yellow','blue'])
-   await page.waitForTimeout(5000);
+   
+//Handle the hidden dropdown
+   await page.locator("input[id='comboBox']").click();
+   await page.waitForSelector("//div[@id='dropdown']/div");
+   
+    const Options= await  page.$$("//div[@id='dropdown']/div");
+
+    for(const option of Options){
+      
+        const value=await option.textContent();
+
+        if(value.includes('Item 21')){
+         await option.click();
+        }
+        
+
+
+        //mahesh chothve
+        //14/08/25
+      
+    }
 })
