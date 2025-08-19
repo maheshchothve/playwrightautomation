ef51e166d9b1bf6e5ac68704e93eeae2fe8c26c8
mahesh chothve <maheshchothve1997@gmail.com>
2025-08-19 22:03:51 +0530

    test cases added and uplod files

---
+++ tests/Single_upload.spec.js
@@ -0,0 +1,34 @@
+const { test, expect } = require('@playwright/test');
+
+test.skip('single upload', async ({ page }) => {
+
+      await  page.goto("https://testautomationpractice.blogspot.com/")
+     const btn = await page.locator("//input[@id='singleFileInput']")
+
+     await btn.setInputFiles('tests/uploadfile/a.txt.docx');
+
+     await page.locator("//button[normalize-space()='Upload Single File']").click();
+
+     await expect(page.locator("//p[@id='singleFileStatus']")).toHaveText("Single file selected: a.txt.docx, Size: 11529 bytes, Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+
+      await  page.waitForTimeout(7000);
+
+});
+
+test('multiple upload', async ({ page }) => {
+
+      await  page.goto("https://testautomationpractice.blogspot.com/")
+     const btn = await page.locator("//input[@id='multipleFilesInput']")
+
+     await btn.setInputFiles(['tests/uploadfile/a.txt.docx','tests/uploadfile/Overseas_Sales_Test_File.csv']);
+
+     await page.locator("//button[normalize-space()='Upload Multiple Files']").click();
+
+     await expect(page.locator("//p[@id='multipleFilesStatus']")).toHaveText(`Multiple files selected:
+a.txt.docx, Size: 11529 bytes, Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document
+Overseas_Sales_Test_File.csv, Size: 5925 bytes, Type: text/csv`)
+
+      await  page.waitForTimeout(7000);
+
+});
+
