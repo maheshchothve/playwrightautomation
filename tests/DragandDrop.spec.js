b504607863b1cf0d9c3abae21d47b5952c9cc503
mahesh chothve <maheshchothve1997@gmail.com>
2025-08-20 15:17:56 +0530

    drag and drop functionlity

---
+++ tests/DragandDrop.spec.js
@@ -0,0 +1,12 @@
+const{test,expect}= require('@playwright/test')
+
+test('drag and drop', async ({ page }) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    const source = await page.locator("//div[@id='draggable']")
+    const target = await page.locator("//div[@id='droppable']")
+
+    await source.dragTo(target)
+
+    await expect(target).toHaveText("Dropped!")
+})
