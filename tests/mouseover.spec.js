0c9daba57325e1a68a8f1c559d1982bcadcba0d8
mahesh chothve <maheshchothve1997@gmail.com>
2025-08-20 16:15:11 +0530

    deleted the previous ss

--- tests/mouseover.spec.js
+++ tests/mouseover.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 test('mosueover', async ({ page }) => {
 
       await  page.goto("https://testautomationpractice.blogspot.com/")
-     const btn = page.locator("//button[@class='dropbtn']")
+     const btn = await page.locator("//button[@class='dropbtn']")
 
      await btn.hover();
 
