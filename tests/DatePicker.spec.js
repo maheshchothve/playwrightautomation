b6179613e122b598db569b0e0e64c87cfab68d62
maheshchothve1509 <maheshchothve1997@gmail.com>
2025-09-04 18:55:34 +0530

    delted the code and adding to first branch

--- tests/DatePicker.spec.js
+++ tests/DatePicker.spec.js
@@ -9,9 +9,3 @@ test('datepicker', async ({ page }) => {
 
 });
 
-// test.only('start and end date days', async ({ page }) => {
-//     await page.goto("https://testautomationpractice.blogspot.com/")
-//     await page.locator("#start-date").fill("12/25/2022")
-//     await page.locator("#end-date").fill("12/31/2022")
-//     await page.waitForTimeout(5000);
-// });
